2639aa636bed153ff891618a9ba7a78bfe373129
Monu kumar <143099632+foodwebsite12@users.noreply.github.com>
2024-02-25 20:10:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,29 @@ function App() {
       <Navbar />
       <div className="cardclass">
         <Card title="Amazon" description="this is description" src="https://images.pexels.com/photos/756789/pexels-photo-756789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <Card title="amazon_logo" description="this is description" src="image/amazon_logo.png" />
+        <Card title="amazon_logo" description="this is description" src="amazon_logo.png" />
         <Card title="box1_image" description="this is description" src="https://images.pexels.com/photos/756789/pexels-photo-756789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <Card title="box2_image" description="this is description" src="image/box2_image.jpg" />
+        <Card title="box2_image" description="this is description" src="box2_image.jpg" />
         <Card title="box3_image" description="this is description" src="https://images.pexels.com/photos/756789/pexels-photo-756789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <Card title="hero_image" description="this is description" src="../image/hero_image.jpg" />
-        <Card title="box4_image" description="this is description" src="image/box4_image.jpg" />
-        <Card title="box5_image" description="this is description" src="image/box5_image.jpg" />
-        <Card title="box6_image" description="this is description" src="image/box6_image.jpg" />
-        <Card title="box7_image" description="this is description" src="image/box7_image.jpg" />
+        <Card title="hero_image" description="this is description" src="hero_image.jpg" />
+        <Card title="box4_image" description="this is description" src="box4_image.jpg" />
+        <Card title="box5_image" description="this is description" src="box5_image.jpg" />
+        <Card title="box6_image" description="this is description" src="box6_image.jpg" />
+        <Card title="box7_image" description="this is description" src="box7_image.jpg" />
         <Card title="box8_image" description="this is description" src="box8_image.jpg" />
-        <Card title="bulkfood" description="this is description" src="image/bulkfood.jpg" />
-        <Card title="car1" description="this is description" src="image/car1.jpg" />
-        <Card title="car2" description="this is description" src="image/car2.jpg" />
-        <Card title="clients3" description="this is description" src="image/clients3.jpg" />
-        <Card title="contact" description="this is description" src="image/contact.jpg" />
-        <Card title="foodcatering" description="this is description" src="image/foodcatering.jpg" />
-        <Card title="foodorder" description="this is description" src="image/foodorder.jpg" />
-        <Card title="google" description="this is description" src="image/google.jpg" />
-        <Card title="hero_image" description="this is description" src="image/hero_image.jpg" />
-        <Card title="microsoft" description="this is description" src="image/microsoft.jpg" />
-        <Card title="pw(1)" description="this is description" src="image/pw.jpg" />
-        <Card title="tasty2" description="this is description" src="image/tasty2.jpg" />
-        <Card title="tastyfood" description="this is description" src="image/tastyfood.jpg" />
+        <Card title="bulkfood" description="this is description" src="bulkfood.jpg" />
+        <Card title="car1" description="this is description" src="car1.jpg" />
+        <Card title="car2" description="this is description" src="car2.jpg" />
+        <Card title="clients3" description="this is description" src="clients3.jpg" />
+        <Card title="contact" description="this is description" src="contact.jpg" />
+        <Card title="foodcatering" description="this is description" src="foodcatering.jpg" />
+        <Card title="foodorder" description="this is description" src="foodorder.jpg" />
+        <Card title="google" description="this is description" src="google.jpg" />
+        <Card title="hero_image" description="this is description" src="hero_image.jpg" />
+        <Card title="microsoft" description="this is description" src="microsoft.jpg" />
+        <Card title="pw(1)" description="this is description" src="pw.jpg" />
+        <Card title="tasty2" description="this is description" src="tasty2.jpg" />
+        <Card title="tastyfood" description="this is description" src="tastyfood.jpg" />
        
         
       </div>
